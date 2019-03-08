e297411d62a802dfc6977aafadbeb000422d112d
yueyaoyao <jiuzhouyise@163.com>
2019-03-08 15:19:35 +0800

    Initial commit

---
+++ src/main/java/com/js/jiangsu/JiangsuApplication.java
@@ -0,0 +1,13 @@
+package com.js.jiangsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiangsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiangsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/js/jiangsu/JiangsuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.js.jiangsu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiangsuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
